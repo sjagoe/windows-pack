b2d2fff35e581435c3f1ec71e5084d903b767bbc
Simon Jagoe <sjagoe@enthought.com>
2013-05-20 10:46:50 +0100

    Make windows-pack safe for loading in non-windows OS

--- init.el
+++ init.el
@@ -1,24 +1,27 @@
 ;; FIXME: Shoudl powershell-mode be in its own pack?
 (live-add-pack-lib "powershell-mode")
 
-(live-load-config-file "windows-path-config.el")
+(require 'powershell-mode)
+(setq auto-mode-alist
+      (append '(("\\.ps1$" . powershell-mode)) auto-mode-alist))
+
 
 (setq this-is-w32
       (or (eq window-system 'w32)
           (equal (string-match "^[A-Za-z]:[/\\]" default-directory) 0)))
 
-(require 'powershell-mode)
-(setq auto-mode-alist
-      (append '(("\\.ps1$" . powershell-mode)) auto-mode-alist))
 
-(require 'tramp)
-(setq tramp-auto-save-directory "c:\\dev\\tmp")
-(setq tramp-default-method "plink")
+(when this-is-w32
+  (live-load-config-file "windows-path-config.el")
 
-;; FIXME: Really in Windows config?
-(setq ediff-split-window-function 'split-window-horizontally)
+  (require 'tramp)
+  (setq tramp-auto-save-directory "c:\\dev\\tmp")
+  (setq tramp-default-method "plink")
 
-(set-default buffer-file-coding-system 'iso-latin-1-unix)
-(set-default-coding-systems 'iso-latin-1-unix)
-(prefer-coding-system 'iso-latin-1-unix)
-(set-default default-buffer-file-coding-system 'iso-latin-1-unix)
+  ;; FIXME: Really in Windows config?
+  (setq ediff-split-window-function 'split-window-horizontally)
+
+  (set-default buffer-file-coding-system 'iso-latin-1-unix)
+  (set-default-coding-systems 'iso-latin-1-unix)
+  (prefer-coding-system 'iso-latin-1-unix)
+  (set-default default-buffer-file-coding-system 'iso-latin-1-unix))
